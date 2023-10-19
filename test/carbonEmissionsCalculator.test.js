d626c5f6429c9090c306fe7f8187d58fd023b336
Enas <enas.elmershati@gmail.com>
2023-10-19 11:46:38 +0200

    :bulb:Comment test

--- test/carbonEmissionsCalculator.test.js
+++ test/carbonEmissionsCalculator.test.js
@@ -1,19 +1,19 @@
-const CarbonEmissions = require("../src/CarbonEmissionsCalculator");
+// const CarbonEmissions = require("../src/CarbonEmissionsCalculator");
 
-test("Calculate emission", () => {
-    const carbonEmissions = new CarbonEmissions();
-    const testData = 100;
-    const testPolicy = { rate: 0.5 };
-    const expectEmission = 50;
-    expect(carbonEmissions.calculateEmission(testData, testPolicy)).toBe(expectEmission);
-});
+// test("Calculate emission", () => {
+//     const carbonEmissions = new CarbonEmissions();
+//     const testData = 100;
+//     const testPolicy = { rate: 0.5 };
+//     const expectEmission = 50;
+//     expect(carbonEmissions.calculateEmission(testData, testPolicy)).toBe(expectEmission);
+// });
 
-test("Calculate emission score", () => {
-    const carbonEmissions = new CarbonEmissions();
-    const testEmission = 50; // Émission de test
-    const testPolicy = { maxEmission: 100 }; // Politique de test
-    const expectedScore = 50; // Score attendu
-    expect(carbonEmissions.calculateEmissionScore(testEmission, testPolicy)).toBe(expectedScore);
-});
+// test("Calculate emission score", () => {
+//     const carbonEmissions = new CarbonEmissions();
+//     const testEmission = 50; // Émission de test
+//     const testPolicy = { maxEmission: 100 }; // Politique de test
+//     const expectedScore = 50; // Score attendu
+//     expect(carbonEmissions.calculateEmissionScore(testEmission, testPolicy)).toBe(expectedScore);
+// });
 
 
